de42203f10e81192a6911e169d94a4c50d91a35a
rishi <hrishikeshknair@gmail.com>
2024-03-11 19:07:42 +0100

    final commit

---
+++ build/self_driving_car_pkg/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/rishi/Desktop/ROS2_self/ROS2-Self-Driving-Car-AI-using-OpenCV/self_driving_car_pkg/install/self_driving_car_pkg'
